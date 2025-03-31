0c0d27926816ceda3dc4cf5346a148b0c88bea04
rabia790 <115765120+rabia790@users.noreply.github.com>
2025-03-31 14:47:56 -0400

    Update authConfig.js

--- src/api/authConfig.js
+++ src/api/authConfig.js
@@ -1,12 +1,7 @@
 // authConfig.js
 import { Platform } from 'react-native';
 
-const authConfig = {
-  issuer: 'https://login.microsoftonline.com/c2883102-3f8d-4e6f-b65a-df3518b3b0f3',
-  clientId: '51079f97-b82a-4ce9-8ace-ecd9d698ca7c',
-  redirectUrl: Platform.OS === 'ios' ? 'msauth.your.bundle.id://auth' : 'yourapp://auth',
-  additionalParameters: {},
-  scopes: ['openid', 'profile', 'email', 'offline_access', 'user.read'],
+
 };
 
 export default authConfig;
